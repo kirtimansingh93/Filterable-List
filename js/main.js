621d732d0847bf351fe2bc1056098c60ee3bc2bd
Kirtiman Singh <kirt93@hotmail.com>
2019-01-10 13:48:50 +0000

    Added event listener to the input element with a callback function filterNames

---
+++ js/main.js
@@ -0,0 +1,8 @@
+// Get input element
+  let filterInput = document.getElementById('filterInput');
+//Add Event Listener (The keyup event occurs when a keyboard key is released.)
+  filterInput.addEventListener('keyup', filterNames);
+
+  function filterNames() {
+  
+  }

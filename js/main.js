24249d977155fdba416d68688be38ae174c4a721
Kirtiman Singh <kirt93@hotmail.com>
2019-01-13 21:10:47 +0000

    In function filterNames, get value of the search input and convert to uppercase

--- js/main.js
+++ js/main.js
@@ -1,8 +1,10 @@
 // Get input element
   let filterInput = document.getElementById('filterInput');
-//Add Event Listener (The keyup event occurs when a keyboard key is released.)
+// Add Event Listener (The keyup event occurs when a keyboard key is released.)
   filterInput.addEventListener('keyup', filterNames);
 
   function filterNames() {
-  
+  // Get value of the search input
+    let filterValue = document.getElementById('filterInput').value.toUpperCase();
+
   }
